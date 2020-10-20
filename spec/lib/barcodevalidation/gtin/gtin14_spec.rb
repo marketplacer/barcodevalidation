acfab453d670dfebd09dcc75876e7e7952b7bc2c
Mark Beattie <beattie.mark@gmail.com>
2020-10-20 12:16:10 +1100

    Test: GTIN-14 conversion down to 8, 12, and 13

--- spec/lib/barcodevalidation/gtin/gtin14_spec.rb
+++ spec/lib/barcodevalidation/gtin/gtin14_spec.rb
@@ -40,4 +40,106 @@ RSpec.describe BarcodeValidation::GTIN::GTIN14 do
       end
     end
   end
+
+  context "converstion" do
+    before do
+      expect(gtin.is_a?(described_class)).to be_truthy
+      expect(gtin).to be_valid
+    end
+
+    context "to GTIN-8" do
+      let(:gtin8) { gtin.to_gtin_8 }
+
+      context "when prefixed with zeros" do
+        let(:input) { "00000012345670" }
+
+        it 'is valid' do
+          expect(gtin8).to be_valid
+        end
+
+        it 'is a BarcodeValidation::GTIN::GTIN8' do
+          expect(gtin8.is_a?(BarcodeValidation::GTIN::GTIN8)).to be_truthy
+        end
+      end
+
+      context "when not prefixed with zeros" do
+        let(:input) { "12345678901231" }
+
+        it 'not valid' do
+          expect(gtin8).to_not be_valid
+        end
+
+        it 'is a BarcodeValidation::InvalidGTIN' do
+          expect(gtin8.is_a?(BarcodeValidation::InvalidGTIN)).to be_truthy
+        end
+      end
+    end
+
+    context "to GTIN-12" do
+      let(:gtin12) { gtin.to_gtin_12 }
+
+      context "when prefixed with zeros" do
+        let(:input) { "00123456789012" }
+
+        it 'is valid' do
+          expect(gtin12).to be_valid
+        end
+
+        it 'is a BarcodeValidation::GTIN::GTIN12' do
+          expect(gtin12.is_a?(BarcodeValidation::GTIN::GTIN12)).to be_truthy
+        end
+      end
+
+      context "when not prefixed with zeros" do
+        let(:input) { "12345678901231" }
+
+        it 'is not valid' do
+          expect(gtin12).to_not be_valid
+        end
+
+        it 'is a BarcodeValidation::InvalidGTIN' do
+          expect(gtin12.is_a?(BarcodeValidation::InvalidGTIN)).to be_truthy
+        end
+      end
+    end
+
+    context "to GTIN-13" do
+      let(:gtin13) { gtin.to_gtin_13 }
+
+      before do
+      end
+
+      context "when prefixed with zeros" do
+        let(:input) { "00123456789012" }
+
+        it 'is valid' do
+          expect(gtin13).to be_valid
+        end
+
+        it 'is a BarcodeValidation::GTIN::GTIN13' do
+          expect(gtin13.is_a?(BarcodeValidation::GTIN::GTIN13)).to be_truthy
+        end
+      end
+
+      context "when not prefixed with zeros" do
+        let(:input) { "12345678901231" }
+
+        it 'is not valid' do
+          expect(gtin13).to_not be_valid
+        end
+
+        it 'is a BarcodeValidation::InvalidGTIN' do
+          expect(gtin13.is_a?(BarcodeValidation::InvalidGTIN)).to be_truthy
+        end
+      end
+    end
+
+    context "to GTIN-14" do
+      let(:input) { "12345678901231" }
+
+      it 'returns itself' do
+        expect(gtin.to_gtin_14).to eq(gtin)
+      end
+    end
+  end
 end
