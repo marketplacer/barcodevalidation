d5bbaa7f93c99456355536016ccda3eaeb5eb491
Ellis Clayton <ellis@ellis.codes>
2022-02-04 09:49:56 +1100

    test: wip

--- release.config.js
+++ release.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  branches: ['main'],
-  tagFormat: 'v${version}',
+  branches: ["main", "ellis/chore/update-semantic-release"],
+  tagFormat: "v${version}-pre",
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
-    'semantic-release-rubygem'
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    "semantic-release-rubygem",
   ],
   ci: process.env.CI || false,
-  dryRun: false
-}
+  dryRun: false,
+};
